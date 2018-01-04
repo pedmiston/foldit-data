58b663c956f6e5a83cbe094242b168e3f53220d9
Pierce Edmiston <pierce.edmiston@gmail.com>
2018-01-04 06:20:13 -0600

    First attempt at saving from db into r pkg"

--- data-raw/use-data.R
+++ data-raw/use-data.R
@@ -1,12 +1,17 @@
 library(devtools)
-library(readr)
 library(dplyr)
 
-TopScores <- read_csv("data-raw/top_scores.csv")
-TopActions <- read_csv("data-raw/top_actions.csv")
+con <- connect_to_db()
 
-use_data(
-  TopScores,
-  TopActions,
-  overwrite = TRUE
-)
+solutions <- tbl(con, "solution") %>%
+  rename(solution_id = id) %>%
+  left_join(tbl(con, "association"))
+
+players <- tbl(con, "player") %>%
+  rename(player_id = id) %>%
+  left_join(tbl(con, "team"))
+
+Solutions <- left_join(solutions, players) %>%
+  collect()
+
+use_data(Solutions, overwrite = TRUE)
